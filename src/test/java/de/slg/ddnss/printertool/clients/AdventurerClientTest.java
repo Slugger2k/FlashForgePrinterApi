43fcfeefd1fba7c978278d41fd7487d2420d1877
Christian Müller <christian.muell3r@gmail.com>
2020-11-19 21:22:42 +0100

    cleaning

--- src/test/java/de/slg/ddnss/printertool/clients/AdventurerClientTest.java
+++ src/test/java/de/slg/ddnss/printertool/clients/AdventurerClientTest.java
@@ -7,22 +7,38 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class AdventurerClientTest {
+	
+	private static String printerAddress;
 
-	@Test
+	@BeforeAll
+	static void init() {
+		try {
+			printerAddress = new FlashForgeDiscoverClient().getPrinterAddress();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+//	@Test
 	void printTest() throws UnknownHostException, IOException {
-		AdventurerClient client = new AdventurerClient(new FlashForgeDiscoverClient().getPrinterAddress());
+		AdventurerClient client = new AdventurerClient(printerAddress);
 		boolean print = client.print(Paths.get("20mm_Box.gx"));
+		client.close();
 		assertTrue(print);
 	}
 
 	
 	@Test
 	void commandTest() throws UnknownHostException, IOException {
-		AdventurerClient client = new AdventurerClient(new FlashForgeDiscoverClient().getPrinterAddress());
+		AdventurerClient client = new AdventurerClient(printerAddress);
 		String replay = client.sendCommand(AdventurerCommands.CMD_INFO_FW);
+		System.out.println(replay);
+		client.close();
 		assertNotNull(replay);
 	}
+	
 }
