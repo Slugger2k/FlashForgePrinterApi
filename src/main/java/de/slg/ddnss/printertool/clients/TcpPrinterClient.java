5c86fe435478754a282ecc6d168cd968ea19fd06
Christian Müller <christian.muell3r@gmail.com>
2020-11-24 00:28:14 +0100

    check reversed

--- src/main/java/de/slg/ddnss/printertool/clients/TcpPrinterClient.java
+++ src/main/java/de/slg/ddnss/printertool/clients/TcpPrinterClient.java
@@ -54,7 +54,7 @@ public class TcpPrinterClient implements Closeable {
 				dos.write(bs);
 				String replay = receiveSingleLineReplay(socket);
 				System.out.println(replay);
-				if (replay.matches("N\\d{4,}\\serror.")) {
+				if (!replay.matches("N\\d{4,}\\sok.")) {
 					throw new FlashForgePrinterTransferException("Error while transfering data.");
 				}
 			}
